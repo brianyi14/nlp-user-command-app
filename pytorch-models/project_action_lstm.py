5ba4005c72596007890b9e8531bd8e3bc7b9af70
baebhav <vmalik.dtp@gmail.com>
2020-11-15 17:59:06 -0500

    changed variable name

--- pytorch-models/project_action_lstm.py
+++ pytorch-models/project_action_lstm.py
@@ -5,11 +5,11 @@ class TopicModeler(nn.Module):
   	def __init__(self):
     	super(TopicModeler,self).__init__()
     	self.LSTM = nn.LSTM(300,100,batch_first = True)
-    	self.output2topic = nn.Linear(100,5) 
+    	self.outputtopic = nn.Linear(100,5) 
     	self.softmax = nn.LogSoftmax(dim=-1)
 
   	def forward(self,input):
     	output, (h_n,c_n) = self.LSTM(input)
-    	topic_output = self.output2topic(h_n)
+    	topic_output = self.outputtopic(h_n)
     	prob_output = self.softmax(topic_output)
     	return prob_output
