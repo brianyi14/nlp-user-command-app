10504200d4da552954c5d5095ab3bb05e499b0d7
baebhav <vmalik.dtp@gmail.com>
2020-11-15 18:50:21 -0500

    changed files

--- pytorch-models/simple.py
+++ pytorch-models/simple.py
@@ -5,6 +5,9 @@ class SimpleModel(nn.Module):
   def __init__(self):
     super(SimpleModel,self).__init__()
     self.llayer = nn.LSTM(300,100,batch_first = True)
+    self.outputtopic = nn.Linear(100,5)
 
   def forward(self,x):
-    return self.llayer(x)
+    outputo = self.llayer(x)
+    outputt = self.outputtopic(outputo)
+    return outputt
