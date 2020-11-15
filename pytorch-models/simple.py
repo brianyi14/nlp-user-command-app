31d0fe70720ad48dadf9144e0da8a5090d1d203e
baebhav <vmalik.dtp@gmail.com>
2020-11-15 17:49:39 -0500

    heh

--- pytorch-models/simple.py
+++ pytorch-models/simple.py
@@ -4,8 +4,7 @@ from torch import nn
 class SimpleModel(nn.Module):
   def __init__(self):
     super(SimpleModel,self).__init__()
-    self.llayer = nn.Linear(5,1)
-    nn.init.xavier_uniform(self.llayer.weight)
+    self.llayer = nn.LSTM(300,100,batch_first = True)
 
   def forward(self,x):
     return self.llayer(x)
