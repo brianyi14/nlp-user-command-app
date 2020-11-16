ee990078b5a5fcce62b4379ad221b30dd3f1a32e
baebhav <vmalik.dtp@gmail.com>
2020-11-15 19:02:33 -0500

    new file

--- pytorch-models/simple.py
+++ pytorch-models/simple.py
@@ -6,8 +6,10 @@ class SimpleModel(nn.Module):
     super(SimpleModel,self).__init__()
     self.llayer = nn.LSTM(300,100,batch_first = True)
     self.outputtopic = nn.Linear(100,5)
+    self.softmax = nn.LogSoftmax(dim=-1)
 
   def forward(self,x):
     outputo = self.llayer(x)
     outputt = self.outputtopic(outputo)
-    return outputt
+    prob_output = self.softmax(outputt)
+    return prob_output
