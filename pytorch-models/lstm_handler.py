7dbb2eb520db87ebb597f0a65ae86a46b5c76207
baebhav <vmalik.dtp@gmail.com>
2020-11-16 16:18:10 -0500

    Updated handler

--- pytorch-models/lstm_handler.py
+++ pytorch-models/lstm_handler.py
@@ -10,14 +10,14 @@ class ModelHandler(BaseHandler):
   	#path = api.load("word2vec-google-news-300", return_path=True)
   	#w = models.KeyedVectors.load_word2vec_format(path, binary=True)
   	stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]  
-  	cleaned_sentence = ''
-  	for char in sentence:
-  		if char.isalpha() or char == ' ':
-      		cleaned_sentence += char
-    	if char == '-':
-      		cleaned_sentence += ' '
-    spell_checker = SpellChecker()
-  	sentence_lst = cleaned_sentence.split()
+  	#cleaned_sentence = ''
+  	#for char in sentence:
+  		#if char.isalpha() or char == ' ':
+      		#cleaned_sentence += char
+    	#if char == '-':
+      		#cleaned_sentence += ' '
+    #spell_checker = SpellChecker()
+  	sentence_lst = data.split()
   	num_words = len(sentence_lst)
   	sentence_vector = np.zeros((max_len,300))
   	for i in range(num_words):
