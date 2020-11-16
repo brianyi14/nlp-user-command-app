6fb35c48c13be4500bd70fa23105870e4be51521
baebhav <vmalik.dtp@gmail.com>
2020-11-15 19:37:41 -0500

    changed everything

--- pytorch-models/project_action_lstm.py
+++ pytorch-models/project_action_lstm.py
@@ -1,15 +1,15 @@
-import torch 
+import torch
 from torch import nn
 
-class TopicModeler(nn.Module):
+class SimpleModel(nn.Module):
   def __init__(self):
-    super(TopicModeler,self).__init__()
-    self.lstm = nn.LSTM(300,100,batch_first = True)
-    self.outputtopic = nn.Linear(100,5) 
+    super(SimpleModel,self).__init__()
+    self.llayer = nn.LSTM(300,100,batch_first = True)
+    self.outputtopic = nn.Linear(100,5)
     self.softmax = nn.LogSoftmax(dim=-1)
 
-  def forward(self,input):
-    output, (h_n,c_n) = self.lstm(input)
-    topic_output = self.outputtopic(h_n)
-    prob_output = self.softmax(topic_output)
+  def forward(self,x):
+    outputo, (h_n,c_n) = self.llayer(x)
+    outputt = self.outputtopic(h_n)
+    prob_output = self.softmax(outputt)
     return prob_output
