d49434fbe96dc3a36ed85fb223404e4e45b7eec9
baebhav <vmalik.dtp@gmail.com>
2020-11-15 19:20:13 -0500

    fixed indent

--- pytorch-models/project_action_lstm.py
+++ pytorch-models/project_action_lstm.py
@@ -2,14 +2,14 @@ import torch
 from torch import nn
 
 class TopicModeler(nn.Module):
-  	def __init__(self):
-    	super(TopicModeler,self).__init__()
-    	self.LSTM = nn.LSTM(300,100,batch_first = True)
-    	self.outputtopic = nn.Linear(100,5) 
-    	self.softmax = nn.LogSoftmax(dim=-1)
+  def __init__(self):
+    super(TopicModeler,self).__init__()
+    self.LSTM = nn.LSTM(300,100,batch_first = True)
+    self.outputtopic = nn.Linear(100,5) 
+    self.softmax = nn.LogSoftmax(dim=-1)
 
-  	def forward(self,input):
-    	output, (h_n,c_n) = self.LSTM(input)
-    	topic_output = self.outputtopic(h_n)
-    	prob_output = self.softmax(topic_output)
-    	return prob_output
+  def forward(self,input):
+    output, (h_n,c_n) = self.LSTM(input)
+    topic_output = self.outputtopic(h_n)
+    prob_output = self.softmax(topic_output)
+    return prob_output
