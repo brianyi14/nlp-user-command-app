34c8af93f5eab4a171300277e13c83384c12e970
baebhav <vmalik.dtp@gmail.com>
2020-11-18 12:18:47 -0500

    hmmmmm

--- torchserve/project_action/project_action_handler.py
+++ torchserve/project_action/project_action_handler.py
@@ -2,13 +2,12 @@ from ts.torch_handler.base_handler import BaseHandler
 import torch
 import numpy as np
 from gensim import models
-import gensim.downloader as api
 from spellchecker import SpellChecker
 
 class ModelHandler(BaseHandler):
   def preprocess(self, data):
-  	path = api.load("word2vec-google-news-300", return_path=True)
-  	w = models.KeyedVectors.load_word2vec_format(path, binary=True)
+  	#path = api.load("word2vec-google-news-300", return_path=True)
+  	#w = models.KeyedVectors.load_word2vec_format(path, binary=True)
   	stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]  
 	  cleaned_sentence = ''
   	for char in sentence:
@@ -24,14 +23,14 @@ class ModelHandler(BaseHandler):
 		word = sentence_lst[i]
     	if word not in stopwords:
       		try:
-        		vectorized_word = w.wv[word]
+        		vectorized_word = np.zeros(300)
       		except KeyError:
         		misspelled = spell_checker.unknown([word])
         	corrected_word = None
         	for word in misspelled:
           		corrected_word = spell_checker.correction(word)
         	try:
-          		vectorized_word = w.wv[corrected_word]
+          		vectorized_word = np.zeros(300)
         	except KeyError:
           		vectorized_word = np.zeros(300)
     	else:
