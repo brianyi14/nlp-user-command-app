845719f630165d2956c0b8ee1f95e9508c41988b
baebhav <vmalik.dtp@gmail.com>
2020-11-21 18:33:18 -0500

    new stuff

--- sentence_vectorizer_api/main.py
+++ sentence_vectorizer_api/main.py
@@ -1,82 +1,57 @@
-import endpoints
-from endpoints import message_types
-from endpoints import messages
-from endpoints import remote
 import numpy as np
 from gensim import models
 import gensim.downloader as api
 from spellchecker import SpellChecker
-
-# [START endpoints_echo_api_messages]
-class EchoRequest(messages.Message):
-    message = messages.StringField(1)
-
-
-class EchoResponse(messages.Message):
-    """A proto Message that contains a simple string field."""
-    message = messages.StringField(1)
+import json
+from json import JSONEncoder
+from flask import Flask,request,render_template
+from flask_cors import CORS, cross_origin
 
 
-ECHO_RESOURCE = endpoints.ResourceContainer(
-    EchoRequest,
-    n=messages.IntegerField(2, default=1))
-# [END endpoints_echo_api_messages]
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
 
-# [START endpoints_echo_api_class]
-@endpoints.api(name='echo', version='v1')
-class EchoApi(remote.Service):
+path = api.load("word2vec-google-news-300", return_path=True)
+w = models.KeyedVectors.load_word2vec_format(path, binary=True)
+@app.route('/',methods = ['POST'])
+@cross_origin()
+def home():
+    data = request.get_json()
+    sentence = data['command']
+    stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+    spell_checker = SpellChecker()
+    sentence_lst = sentence.split()
+    num_words = len(sentence_lst)
+    sentence_vector = np.zeros((10,300))
+    for i in range(num_words):
+        word = sentence_lst[i]
+        if word not in stopwords:
+            try:
+                vectorized_word = w.wv[word]
+            except:
+                misspelled = spell_checker.unknown([word])
+                corrected_word = None
+                for word in misspelled:
+                    corrected_word = spell_checker.correction(word)
+                try:
+                    vectorized_word = w.wv[corrected_word]
+                except:
+                    vectorized_word = np.zeros(300)
+        else:
+            vectorized_word = np.zeros(300)
+        sentence_vector[i] = vectorized_word
+    for j in range(num_words,10,1):
+        sentence_vector[j] = np.zeros(300)
+    numpyData = {"array": sentence_vector}
+    output = json.dumps(numpyData, cls=NumpyArrayEncoder)
+    return output
 
-    # [START endpoints_echo_api_method]
-    @endpoints.method(
-        # This method takes a ResourceContainer defined above.
-        ECHO_RESOURCE,
-        # This method returns an Echo message.
-        EchoResponse,
-        path='echo',
-        http_method='POST',
-        name='echo')
-    def echo(self, request):
-        output_message = ' '.join([request.message] * request.n)
-        return EchoResponse(message=output_message)
-    # [END endpoints_echo_api_method]
-
-    @endpoints.method(
-        # This method takes a ResourceContainer defined above.
-        ECHO_RESOURCE,
-        # This method returns an Echo message.
-        EchoResponse,
-        path='echo/{n}',
-        http_method='POST',
-        name='echo_path_parameter')
-    def echo_path_parameter(self, request):
-        sentence = request.message
-		spell_checker = SpellChecker()
-  		sentence_lst = sentence.split()
-  		num_words = len(sentence_lst)
-  		sentence_vector = np.zeros((10,300))
-  		for i in range(num_words):
-    		word = sentence_lst[i]
-    		if word not in stopwords:
-      			try:
-        			vectorized_word = vectorizer.wv[word]
-      			except:
-        			misspelled = spell_checker.unknown([word])
-        		corrected_word = None
-        		for word in misspelled:
-          			corrected_word = spell_checker.correction(word)
-        			try:
-          				vectorized_word = vectorizer.wv[corrected_word]
-        			except:
-          				vectorized_word = np.zeros(300)
-    		else:
-      			vectorized_word = np.zeros(300)
-    		sentence_vector[i] = vectorized_word
-  		for j in range(num_words,10,1):
-    		sentence_vector[j] = np.zeros(300)
-  		output_message = sentence_vector.tostring()
-        return EchoResponse(message=output_message)
-
-# [START endpoints_api_server]
-api = endpoints.api_server([EchoApi])
-# [END endpoints_api_server]
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8080,debug=True)
