c5d3bb6ec00c8645a28bd9a554a52f706cf19a93
baebhav <vmalik.dtp@gmail.com>
2020-11-21 01:06:09 -0500

    i think this will work

--- sentence_vectorizer_api/main.py
+++ sentence_vectorizer_api/main.py
@@ -7,6 +7,7 @@ from gensim import models
 import gensim.downloader as api
 from spellchecker import SpellChecker
 
+# [START endpoints_echo_api_messages]
 class EchoRequest(messages.Message):
     message = messages.StringField(1)
 
@@ -16,14 +17,66 @@ class EchoResponse(messages.Message):
     message = messages.StringField(1)
 
 
-ECHO_RESOURCE = endpoints.ResourceContainer(EchoRequest,n=messages.IntegerField(2, default=1))
+ECHO_RESOURCE = endpoints.ResourceContainer(
+    EchoRequest,
+    n=messages.IntegerField(2, default=1))
+# [END endpoints_echo_api_messages]
 
-@endpoints.api(name='vectorizer', version='v1')
-class VectorizerApi(remote.Service):
-    @endpoints.method(ECHO_RESOURCE,EchoResponse,path='echo',http_method='POST',name='echo')
+
+# [START endpoints_echo_api_class]
+@endpoints.api(name='echo', version='v1')
+class EchoApi(remote.Service):
+
+    # [START endpoints_echo_api_method]
+    @endpoints.method(
+        # This method takes a ResourceContainer defined above.
+        ECHO_RESOURCE,
+        # This method returns an Echo message.
+        EchoResponse,
+        path='echo',
+        http_method='POST',
+        name='echo')
     def echo(self, request):
+        output_message = ' '.join([request.message] * request.n)
+        return EchoResponse(message=output_message)
+    # [END endpoints_echo_api_method]
+
+    @endpoints.method(
+        # This method takes a ResourceContainer defined above.
+        ECHO_RESOURCE,
+        # This method returns an Echo message.
+        EchoResponse,
+        path='echo/{n}',
+        http_method='POST',
+        name='echo_path_parameter')
+    def echo_path_parameter(self, request):
         sentence = request.message
-	    output_message = len(sentence)
+		spell_checker = SpellChecker()
+  		sentence_lst = sentence.split()
+  		num_words = len(sentence_lst)
+  		sentence_vector = np.zeros((10,300))
+  		for i in range(num_words):
+    		word = sentence_lst[i]
+    		if word not in stopwords:
+      			try:
+        			vectorized_word = vectorizer.wv[word]
+      			except:
+        			misspelled = spell_checker.unknown([word])
+        		corrected_word = None
+        		for word in misspelled:
+          			corrected_word = spell_checker.correction(word)
+        			try:
+          				vectorized_word = vectorizer.wv[corrected_word]
+        			except:
+          				vectorized_word = np.zeros(300)
+    		else:
+      			vectorized_word = np.zeros(300)
+    		sentence_vector[i] = vectorized_word
+  		for j in range(num_words,10,1):
+    		sentence_vector[j] = np.zeros(300)
+  		output_message = sentence_vector.tostring()
         return EchoResponse(message=output_message)
 
-api = endpoints.api_server([VectorizerApi])
+# [START endpoints_api_server]
+api = endpoints.api_server([EchoApi])
+# [END endpoints_api_server]
