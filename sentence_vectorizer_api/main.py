62b091468dfec3be6e002d11be8d01dc9c4cafe8
baebhav <vmalik.dtp@gmail.com>
2020-11-21 00:06:27 -0500

    updated topic modeler

--- sentence_vectorizer_api/main.py
+++ sentence_vectorizer_api/main.py
@@ -20,14 +20,7 @@ ECHO_RESOURCE = endpoints.ResourceContainer(EchoRequest,n=messages.IntegerField(
 
 @endpoints.api(name='vectorizer', version='v1')
 class VectorizerApi(remote.Service):
-    @endpoints.method(
-        # This method takes a ResourceContainer defined above.
-        ECHO_RESOURCE,
-        # This method returns an Echo message.
-        EchoResponse,
-        path='echo',
-        http_method='POST',
-        name='echo')
+    @endpoints.method(ECHO_RESOURCE,EchoResponse,path='echo',http_method='POST',name='echo')
     def echo(self, request):
         sentence = request.message
         stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]  
