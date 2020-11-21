450be0a7702a4b3096e7e60b6e2ce9a4fdd93244
baebhav <vmalik.dtp@gmail.com>
2020-11-20 20:28:31 -0500

    updated Topic Modeler

--- sentence_vectorizer_api/main.py
+++ sentence_vectorizer_api/main.py
@@ -16,9 +16,7 @@ class EchoResponse(messages.Message):
     message = messages.StringField(1)
 
 
-ECHO_RESOURCE = endpoints.ResourceContainer(
-    EchoRequest,
-    n=messages.IntegerField(2, default=1))
+ECHO_RESOURCE = endpoints.ResourceContainer(EchoRequest,n=messages.IntegerField(2, default=1))
 
 @endpoints.api(name='vectorizer', version='v1')
 class VectorizerApi(remote.Service):
@@ -35,7 +33,7 @@ class VectorizerApi(remote.Service):
         stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]  
 	    cleaned_sentence = ''
   	    for char in sentence:
-  		    if char.isalpha() or char == ' ':
+  			if char.isalpha() or char == ' ':
       		    cleaned_sentence += char
     	    if char == '-':
       		    cleaned_sentence += ' '
