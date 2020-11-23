608170ebf5d9d2d6a0422356e5543b3de542d0fb
baebhav <vmalik.dtp@gmail.com>
2020-11-23 18:05:04 -0500

    plz

--- torchserve/project_action/project_action_handler.py
+++ torchserve/project_action/project_action_handler.py
@@ -2,12 +2,10 @@ from ts.torch_handler.base_handler import BaseHandler
 import torch
 import numpy as np
 import requests
-import json
 
 class ModelHandler(BaseHandler):
   def preprocess(self, data):
-	  request_data = json.loads(data)
-    text_command = request_data['command']
+	  text_command = 'Finished with project data cleaning'
     payload = {'command':text_command}
     r = requests.post("https://user-command-nlp.ue.r.appspot.com", json = payload)
     response = r.json()
