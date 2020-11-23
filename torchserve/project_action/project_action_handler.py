73ce4341929aab5d10f9f7d8d14f9cd4da6772ef
baebhav <vmalik.dtp@gmail.com>
2020-11-22 23:32:15 -0500

    heh

--- torchserve/project_action/project_action_handler.py
+++ torchserve/project_action/project_action_handler.py
@@ -6,11 +6,20 @@ import json
 
 class ModelHandler(BaseHandler):
   def preprocess(self, data):
-	  sentence_vector = np.random.rand(10,300)
+	  """
+    request_data = json.loads(data)
+    text_command = request_data['command']
+    payload = {'command':text_command}
+    r = requests.post("https://user-command-nlp.ue.r.appspot.com", json = payload)
+    response = r.json()
+    encodedNumpyData = response['array']
+    sentence_vector = np.asarray(encodedNumpyData)
     tensor_sentence = torch.tensor(sentence_vector)
     model_input = torch.unsqueeze(tensor_sentence,0)
     model_input = model_input.float()
     return model_input
+    """
+    return torch.randn(1,10,300).float()
 
   def postprocess(self, model_pred):
     index2label = {0: 'Create',1: 'On Target',2: 'At Risk',3: 'Danger',4: 'Completed'}
