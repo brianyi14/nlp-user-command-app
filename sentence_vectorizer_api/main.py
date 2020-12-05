d40f33095e54b913d3189b62610ba46f7988f5c9
baebhav <vmalik.dtp@gmail.com>
2020-12-05 00:52:46 -0500

    yea

--- sentence_vectorizer_api/main.py
+++ sentence_vectorizer_api/main.py
@@ -23,11 +23,6 @@ class NumpyArrayEncoder(JSONEncoder):
 vectorizer = models.KeyedVectors.load_word2vec_format(
     "./data/google-word2vec.bin", binary=True)
 
-word_vectors = vectorizer.wv
-word_vectors.save("word2vec.wordvectors")
-
-vectorizer =models.KeyedVectors.load("word2vec.wordvectors", mmap='r')
-
 @app.route('/', methods=['POST'])
 @cross_origin()
 def home():
